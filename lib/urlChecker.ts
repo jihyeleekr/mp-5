65b40dd09139e4ce0132b0e375d2864f81a4a9ea
Jihye Lee <jh020211@bu.edu>
2025-04-17 20:52:03 -0400

    update

--- lib/urlChecker.ts
+++ lib/urlChecker.ts
@@ -1,12 +1,12 @@
 "use server";
 
-export default async function urlChecker(url: string) : Promise<boolean> {
-    try{
-        const res = await fetch(url);
-        return res.status >= 200 && res.status < 400;
-    }
-    catch(err) {
-        console.error(err);
+export default async function checkURL(url: string): Promise<boolean> {
+    const pattern = /^(https?:\/\/)[^\s/$.?#].[^\s]*$/i;
+
+    try {
+        new URL(url);
+        return pattern.test(url);
+    } catch {
         return false;
     }
 }
